48fa68a2d358139114d2626ad712dd9f19dcaa3b
QueiroVielma <queigerar@gmail.com>
2024-09-18 14:12:41 -0300

    Create modal contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class contact (models.Model):
+
+    first_name = models.CharField(max_length=50)
+    Last_name= models.CharField(max_length=50, blank= True)
+    phone= models.CharField(max_length=50)
+    email= models.CharField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    decription= models.TextField(blank=True)
