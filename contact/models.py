f396e6b3c0f51f23dcde41723561606ea1a2d846
QueiroVielma <queigerar@gmail.com>
2024-10-04 10:26:39 -0300

    Create category and class meta

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+class Category (models.Model):
+    class Meta: 
+        verbose_name='Categoria'
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 class contact (models.Model):
 
     first_name = models.CharField(max_length=50)
@@ -11,6 +18,7 @@ class contact (models.Model):
     decription= models.TextField(blank=True)
     show= models.BooleanField(default=True)
     picture=  models.ImageField(blank=True, upload_to='picture/%Y/%m/')
+    category=models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.Last_name}'
