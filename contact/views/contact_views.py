121c9df89f2192477622f4aabb1eeba2c4fa1c0b
QueiroVielma <queigerar@gmail.com>
2024-10-09 15:41:05 -0300

    Create partial files

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -5,7 +5,8 @@ def index (request):
     contacts= contact.objects.filter(show=True).order_by('-id')
 
     data={
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': "contactos - "
     }
 
     return render(
@@ -18,7 +19,8 @@ def contact_met (request, contact_id):
     #single_contact= contact.objects.get(pk=contact_id)
     single_contact= get_object_or_404(contact, pk= contact_id, show=True )
     data={
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title':  f'{single_contact.first_name} {single_contact.Last_name} - '
     }
 
     return render(
